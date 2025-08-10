29f1a821423167a7a71c438756a2b941dbcc0e8a
JeyZ9 <ggjj4511@gmail.com>
2025-08-10 17:32:51 +0700

    fix jenkinsfile

--- index.test.js
+++ index.test.js
@@ -1,9 +1,10 @@
-const request = require("supertest");
-const app = require("./index");
+const request = require('supertest');
+const app = require('./index');
 
-describe("GET /", () => {
-  it("should return 200 OK", async () => {
-    const res = await request(app).get("/");
+describe('GET /', () => {
+  it('should return 200 OK and Hello World', async () => {
+    const res = await request(app).get('/');
     expect(res.statusCode).toBe(200);
+    expect(res.text).toBe('Hello World');
   });
 });
